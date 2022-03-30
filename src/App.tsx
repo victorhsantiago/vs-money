4341ce5d0d57a0094a3e15ce6e6c7cd05febe334
Victor Santiago <victorhsantiago3@gmail.com>
2022-03-29 23:35:26 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { GlobalStyle } from "./style/global"
+
+
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>VS Money</h1>
+      <button>VS Money</button>
     </div>
   );
 }
---
+++ src/style/global.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --blue: #5429CC;
+    --dark-blue: #363F5F;
+    --light-blue: #6933ff;
+    
+    --gray: #969CB3;
+    --light-gray: #F0F2F5;
+    
+    --green: #33CC95;
+    
+    --red: #E62E4D;
+    
+    --wihe: #FFFFFF;
+  }
+
+  * {
+    margin: 0;
+    padding: 0 ;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  html {
+    @media (min-width: 720px) {
+      font-size: 87.5%;
+    }
+
+    @media (min-width: 1080px) {
+      font-size: 93.75%;
+    }
+  }
+
+  body {
+    background: var(--light-gray);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-weight: 400
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600
+  }
+
+  button  {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
