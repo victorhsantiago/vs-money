77be65faeda0e6c324da067a928db38f7ac9349b
Victor Santiago <victorhsantiago3@gmail.com>
2022-04-02 19:41:07 -0300

    build: add axios and miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
+import { createServer } from 'miragejs'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return []
+    })
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
